05fc2a5fbfe0914a82b48ca209894311e55492cd
NikoRozhnik <niko.rozhnik@gmail.com>
2022-04-17 11:03:42 +0300

    modified db.py to support multiple threads

--- jambot/db.py
+++ jambot/db.py
@@ -11,30 +11,29 @@ HAS_ONE_TEMPL = "SELECT ID FROM {} WHERE user_id = ? and id = ?"
 class DataBaseAPI:
     def __init__(self, db_name):
         self.db_name = db_name
-        self.con = sqlite3.connect(self.db_name)
         self.table_col_names = {
             "artists": self._get_column_names("artists"),
             "albums": self._get_column_names("albums"),
             "tracks": self._get_column_names("tracks"),
         }
 
+    def _connect(self):
+        return sqlite3.connect(self.db_name)
+
     def _get_column_names(self, table_name):
-        cur = self.con.cursor()
         query = GET_COL_NAMES_TEMPL.format(table_name)
-        cur.execute(query)
-        res = [descr[0] for descr in cur.description if descr[0] != "user_id"]
-        self.con.rollback()
-        cur.close()
+        with self._connect() as con:
+            cur = con.cursor()
+            cur.execute(query)
+            res = [descr[0] for descr in cur.description if descr[0] != "user_id"]
+            cur.close()
         return res
 
     def _do_get_all(self, table_name, user_id):
         col_names = self.table_col_names[table_name]
         query = GET_ALL_TEMPL.format(", ".join(col_names), table_name)
-        print(query)
-        cur = self.con.cursor()
-        res = [dict(zip(col_names, row)) for row in cur.execute(query, (user_id,)).fetchall()]
-        self.con.rollback()
-        cur.close()
+        with self._connect() as con:
+            res = [dict(zip(col_names, row)) for row in con.execute(query, (user_id,)).fetchall()]
         return res
 
     def _do_add_one(self, table_name, user_id, item):
@@ -46,24 +45,18 @@ class DataBaseAPI:
         for cn in col_names:
             p_values.append(params[cn])
         query = ADD_ONE_TEMPL.format(table_name, ", ".join(col_names), ", ".join(["?"] * len(col_names)))
-        cur = self.con.cursor()
-        cur.execute(query, p_values)
-        self.con.commit()
-        cur.close()
+        with self._connect() as con:
+            con.execute(query, p_values)
 
     def _do_del_one(self, table_name, user_id, id):
         query = DEL_ONE_TEMPL.format(table_name)
-        cur = self.con.cursor()
-        cur.execute(query, (user_id, id))
-        self.con.commit()
-        cur.close()
+        with self._connect() as con:
+            con.execute(query, (user_id, id))
 
     def _do_has_one(self, table_name, user_id, id):
         query = HAS_ONE_TEMPL.format(table_name)
-        cur = self.con.cursor()
-        res = len(cur.execute(query, (user_id, id)).fetchall())
-        self.con.rollback()
-        cur.close()
+        with self._connect() as con:
+            res = len(con.execute(query, (user_id, id)).fetchall())
         return bool(res)
 
     def get_fav_artists(self, user_id):
