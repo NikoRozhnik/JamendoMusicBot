61bd7004e81407a1e7b154b02854dd1aba510d76
NikoRozhnik <niko.rozhnik@gmail.com>
2022-04-17 10:40:07 +0300

    Commands /start and /help translated to russian

--- jambot/commands.py
+++ jambot/commands.py
@@ -5,24 +5,25 @@ from .controls import TrackList, AlbumList, ArtistList, ltFIND, ltFAV
 
 
 wellcome_txt = emoji.emojize(
-    """*Wellcome!*
+    """*Привет!*
 
-I'm *jamendo musical bot* :musical_score:
+Я - *jamendo musical bot* :musical_score:
 
-I will help you find music on [Jamendo Music](http://www.jamendo.com/) and save your favorites artists, albums and tracks
+Я помогу тебе искать музыку на [Jamendo Music](http://www.jamendo.com/)
+ и сохранять списки любимых исполнителей, альбомов и записей
 """
 )
 
 help_txt = emoji.emojize(
     """jamendo musical bot :musical_score: commands:
 
-/help - this help
-/artists <search_str> - find artists
-/albums <search_str> - find albums
-/tracks <search_str> - find tracks
-/fav_artists - show favorite artist list
-/fav_albums - show favorite album list
-/fav_tracks - show favorite track list
+/help - справка о командах
+/artists <search_str> - найти исполнителей
+/albums <search_str> - найти альбомы
+/tracks <search_str> - найти записи
+/fav_artists - список избранных исполнителей
+/fav_albums - список избранных альбомов
+/fav_tracks - список избранныз команд
 """
 )
 
@@ -57,21 +58,21 @@ def find_tracks(update, context):
 
 
 def fav_artists(update, context):
-    user_id = update.effective_user
+    user_id = update.effective_user["id"]
     artists = c.dbAPI.get_fav_artists(user_id)
     data = ArtistList.create(items=artists, user_id=user_id, list_type=ltFAV)
-    # ArtistList.send_message(data)
+    update.message.reply_text(**ArtistList.build_message_attrs(data))
 
 
 def fav_albums(update, context):
-    user_id = update.effective_user
+    user_id = update.effective_user["id"]
     albums = c.dbAPI.get_fav_artists(user_id)
     data = AlbumList.create(items=albums, user_id=user_id, list_type=ltFAV)
-    # AlbumList.send_message(data)
+    update.message.reply_text(**AlbumList.build_message_attrs(data))
 
 
 def fav_tracks(update, context):
-    user_id = update.effective_user
+    user_id = update.effective_user["id"]
     tracks = c.dbAPI.get_fav_tracks(user_id)
     data = TrackList.create(items=tracks, user_id=user_id, list_type=ltFAV)
-    # TrackList.send_message(data)
+    update.message.reply_text(**TrackList.build_message_attrs(data))
