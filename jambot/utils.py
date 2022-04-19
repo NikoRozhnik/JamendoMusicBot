e0c41e4a9f44c8fa09d7419653bcb15e74b6d5e1
NikoRozhnik <niko.rozhnik@gmail.com>
2022-04-20 01:02:04 +0300

    thumbnail image resizing added

--- jambot/utils.py
+++ jambot/utils.py
@@ -1,12 +1,18 @@
 from io import BytesIO
+
 import requests
+from PIL import Image
+
+THUMB_SIZE = (100, 100)
 
 
 def get_thumb(link):
     try:
-        r = requests.get(link)
-        thumb = BytesIO(r.raw.read())
-        return thumb
-    except Exception as e:
-        print("?!" + str(e))
+        r = requests.get(link, stream=True)
+        img = Image.open(BytesIO(r.content))
+        img.thumbnail(THUMB_SIZE)
+        img_bytes = BytesIO()
+        img.save(img_bytes, format="JPEG")
+        return img_bytes.getvalue()
+    except Exception:
         return None
